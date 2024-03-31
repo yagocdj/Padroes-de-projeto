7dd97e89124070727fdb647a4838b1527dc40de9
Yago César <yagocdj@gmail.com>
2024-03-31 00:59:25 -0300

    adicionando mais casos no arquivo main

--- Parking/src/br/edu/ifpb/pps/parking/app/ParkingApp.java
+++ Parking/src/br/edu/ifpb/pps/parking/app/ParkingApp.java
@@ -3,7 +3,9 @@ package br.edu.ifpb.pps.parking.app;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import br.edu.ifpb.pps.parking.calculation.DayParkingBill;
 import br.edu.ifpb.pps.parking.calculation.HourParkingBill;
+import br.edu.ifpb.pps.parking.calculation.MonthParkingBill;
 import br.edu.ifpb.pps.parking.manager.Parking;
 import br.edu.ifpb.pps.parking.vehicle.Vehicle;
 
@@ -17,11 +19,32 @@ public class ParkingApp {
 		parking.setEntryTime(LocalDateTime.parse(
 				"2024-03-31 01:30", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
 		parking.setExitTime(LocalDateTime.parse(
-				"2024-03-31 02:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+				"2024-03-31 04:30", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
 		parking.setVehicle(new Vehicle("ABC1D23", "Civic", "Red"));
 		
 		// set the strategy to be a hour bill calculation
 		parking.setBillCalculation(new HourParkingBill());
+		
+		System.out.println("R$" + parking.calculateBill().doubleValue());
+		
+		// change the parking entry time and exit time
+		parking.setEntryTime(LocalDateTime.parse(
+				"2024-03-30 11:30", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+		parking.setExitTime(LocalDateTime.parse(
+				"2024-03-31 12:30", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+		
+		// set the strategy to be a day bill calculation
+		parking.setBillCalculation(new DayParkingBill());
+		
+		System.out.println("R$" + parking.calculateBill().doubleValue());
+		
+		// change the parking entry time and exit time once more
+		parking.setEntryTime(LocalDateTime.parse(
+				"2024-03-15 11:30", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+		parking.setExitTime(LocalDateTime.parse(
+				"2024-03-31 11:30", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+		// set the strategy to be a month bill calculation
+		parking.setBillCalculation(new MonthParkingBill());
 		
 		System.out.println("R$" + parking.calculateBill().doubleValue());
 	}
